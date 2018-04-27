727a0282b0597ab59026aa24230923144aaad987
Emily Fortuna <efortuna@google.com>
2018-04-27 12:09:28 -0700

    removed the initState

--- lib/main.dart
+++ lib/main.dart
@@ -52,11 +52,6 @@ class FishPageState extends State<FishPage> {
   ViewType _viewType = ViewType.available;
 
   @override
-  initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     var filteredFish = widget.allFish;
     return Scaffold(
